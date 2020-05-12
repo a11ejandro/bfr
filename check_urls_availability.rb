fb2fd938c589e66920eb901ac5431b1c5da25db6
Alexander Potrakhov <bendthe@gmail.com>
2020-05-12 16:28:00 +0300

    Process console arguments (--file and --threads)

--- check_urls_availability.rb
+++ check_urls_availability.rb
@@ -1,8 +1,37 @@
+require 'getoptlong'
 require_relative 'url_checker'
 require_relative 'url_extractor'
 
 file = 'websites_to_check.csv'
 threads = 2
+
+opts = GetoptLong.new(
+  [ '--help', '-h', GetoptLong::NO_ARGUMENT ],
+  [ '--file', '-f', GetoptLong::OPTIONAL_ARGUMENT ],
+  [ '--threads', '-t', GetoptLong::OPTIONAL_ARGUMENT]
+)
+
+opts.each do |opt, arg|
+  case opt
+    when '--help'
+      puts <<-EOF
+        hello [OPTION] ... DIR
+
+        --help, -h:
+        Shows help. Oh, you already know it:-)
+
+        --file [path], -f [path]:
+        Path to csv file with urls. Default value is 'websites_to_check.csv'.
+
+        --threads x:
+        Number of threads to run the checks simultaneously. Default value is 2.
+      EOF
+    when '--file'
+      file = arg
+    when '--threads'
+      threads = arg.to_i
+  end
+end
 
 all_urls = UrlExtractor.new(file: file).result_urls
 group_size = (all_urls.count / threads.to_f).ceil
